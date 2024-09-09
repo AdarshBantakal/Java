c32cf41ca2aa18f86af6ed62f98aa95cddf838be
AdarshBantakal <163523007+AdarshBantakal@users.noreply.github.com>
2024-09-09 19:02:06 +0530

    Update diamondpatern.java

--- src/diamondpatern.java
+++ src/diamondpatern.java
@@ -2,13 +2,14 @@ public class diamondpatern {
  public static void main(String[] args) {
   String s1,s2;
   s1="ABCDEF";
-  for(int i=0;i<7;i++){
+  int len=s1.length();
+  for(int i=0;i<len;i++){
 
     s2 = s1.substring(0,i);
     System.out.println(s2);
    }
 
-  for(int i=5;i>0;i--){
+  for(int i=len;i>0;i--){
    s2=s1.substring(0,i);
    System.out.println(s2);
   }
