8440f4cd984f4b74332eba1fea58e8de17ef9ad3
AdarshBantakal <163523007+AdarshBantakal@users.noreply.github.com>
2024-11-05 15:45:25 +0530

    Add files via upload

---
+++ src/M7.java
@@ -0,0 +1,18 @@
+class Tools2{
+    String s1="";
+    String s2="";
+    int len1=0;
+    public String reverse(String s1) {
+        len1=s1.length();
+        for(int i=0;i<len1;i++) {
+            s2=s2+s1.substring(len1-(i+1),len1-i);
+        }
+        return s2;
+    }
+}
+public class M7 {
+    public static void main(String[] args) {
+        Tools2 t1=new Tools2();
+        System.out.println(t1.reverse("Adarsh"));
+    }
+}
