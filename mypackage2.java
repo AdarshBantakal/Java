3b827da98967fc9491159ccf20ff47476ace18aa
Adarsh Devadiga <163523007+AdarshBantakal@users.noreply.github.com>
2024-12-18 12:40:07 +0530

    Add files via upload

---
+++ mypackage2.java
@@ -0,0 +1,10 @@
+
+
+package pkg3;
+
+public class mypackage2 {
+    public void display()
+    {
+        System.out.println("This msg is from MyPackage class within pkg2");
+    }
+}
---
+++ mypackage3.java
@@ -0,0 +1,8 @@
+
+package pkg3;
+public class mypackage3 {
+    public void display()
+    {
+        System.out.println("This msg is from MyPackage class within pkg3");
+    }
+}
